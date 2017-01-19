5e418778a5a47e86ee7f6a8b4b5c9d11f17f9e98
yoel <yo3l.m18@gmail.com>
2017-01-19 10:17:17 -0400

    where &

--- appRoot/com.zippyttech.rest/restController.ts
+++ appRoot/com.zippyttech.rest/restController.ts
@@ -375,14 +375,14 @@ export class RestController implements OnInit {
     loadDataWhere(id='',where:IWhere=[]){
         let that = this;
         this.findData = true;
-        this.where="?where="+encodeURI(JSON.stringify(where).split('{').join('[').split('}').join(']'));
+        this.where="&where="+encodeURI(JSON.stringify(where).split('{').join('[').split('}').join(']'));
         let successCallback= response => {
             that.findData=false;
             Object.assign(that.dataList, response.json());
             if(!(id && id==''))
                 that.loadPager(that.dataList);
         };
-        return this.httputils.doGet(this.endpoint+id+this.where,successCallback,this.error);
+        return this.httputils.doGet(this.endpoint+id+'?offset=0'+this.where,successCallback,this.error);
     }
 
     changeOrder(sort){
