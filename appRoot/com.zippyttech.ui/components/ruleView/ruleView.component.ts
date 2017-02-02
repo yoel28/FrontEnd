99229ff97fc1da9872f9cd6eaf8e3da1bbe8f428
yoel <yo3l.m18@gmail.com>
2017-02-02 11:14:18 -0400

    typeOf

--- appRoot/com.zippyttech.ui/components/ruleView/ruleView.component.ts
+++ appRoot/com.zippyttech.ui/components/ruleView/ruleView.component.ts
@@ -120,11 +120,13 @@ export class RuleViewComponent implements OnInit,AfterViewInit {
             event.preventDefault();
         this.paramsData.viewListData['title'] = this.model.rules[key].title;
         this.paramsData.viewListData['label']={};
-        if(data[key][0])
+        if(typeof data[key] == 'object' && data[key].length>0)
+        {
             Object.keys(data[key][0]).forEach(subkey=>{
                 if(that.model.rules[key].rulesSave[subkey])
                     that.paramsData.viewListData['label'][subkey] = that.model.rules[key].rulesSave[subkey].title;
             });
+        }
         this.paramsData.viewListData['data'] =  data[key];
         if(typeof data[key] === 'string'){
             this.paramsData.viewListData['data'] = JSON.parse(data[key])
