48a387d79615ce690bdb93430c43372c89565237
yoel <yo3l.m18@gmail.com>
2017-02-09 14:14:24 -0400

    BasicComponent

---
+++ appRoot/com.zippyttech.common/basicComponent.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {BaseViewInstance} from "../com.zippyttech.ui/view/base/baseView.instance";
+import {DependenciesBase} from "./DependenciesBase";
+import {ActivatedRoute} from "@angular/router";
+
+declare var SystemJS:any;
+@Component({
+    selector: 'basic-controller',
+    templateUrl:SystemJS.map.app+'/com.zippyttech.ui/view/base/base.html',
+    styleUrls: [SystemJS.map.app+'/com.zippyttech.ui/view/base/style.css'],
+})
+export class BasicComponent extends BaseViewInstance{
+
+    constructor(public db:DependenciesBase,private route: ActivatedRoute) {
+        super();
+    }
+    initModel() {
+        let data:any = this.route.snapshot.data;
+        if(data.model)
+            this.model= new data.model(this.db);
+    }
+    initViewOptions() {
+        this.viewOptions["title"] = this.model.ruleObject.title;
+    }
+    loadParamsTable(){
+        this.paramsTable.actions={};
+        this.paramsTable.actions.delete = this.model.getParamsDelete;
+    }
+}
