135f45b3658dff553d7f05b28d6f77d4a210665a
yoel <yo3l.m18@gmail.com>
2017-02-20 13:55:02 -0400

    adminLTE

--- appRoot/main.ts
+++ appRoot/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {AppModule} from "./app.module";
 
+var adminLTE = require('adminLTE');
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then(success => console.log("Bootstrap success main"))
   .catch(error => console.log(error));
