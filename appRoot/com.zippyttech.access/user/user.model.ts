eb7e719bb36ead2a545e782095c8604e9dd857bb
yoel <yo3l.m18@gmail.com>
2017-02-22 15:10:14 -0400

    key

--- appRoot/com.zippyttech.access/user/user.model.ts
+++ appRoot/com.zippyttech.access/user/user.model.ts
@@ -158,6 +158,7 @@ export class UserModel extends ModelBase{
         this.ruleObject.title="Usuario";
         this.ruleObject.placeholder="Ingrese el usuario";
         this.ruleObject.keyDisplay='user';
+        this.ruleObject.key='user';
         this.ruleObject.eval=this.db.myglobal.getRule('USER_DISPLAY_WEB');
         this.ruleObject.code="userId";
     }
