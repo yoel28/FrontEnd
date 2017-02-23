7c9caa199071076fa142a900de98ea827314f720
yoel <yo3l.m18@gmail.com>
2017-02-23 17:04:43 -0400

    clear buil

--- clear.sh
+++ clear.sh
@@ -4,5 +4,7 @@ find ./appRoot -type f -name '*.js.map' -exec rm -f {} \;
 find ./appRoot -type f -name '*.d.ts' -exec rm -f {} \;
 rm -f ./system-config.js;
 rm -f ./system-config.js.map;
+rm -f ./build.js.map;
+rm -f ./build.js.map;
 rm -rf dist/
 rm -rf aot/
