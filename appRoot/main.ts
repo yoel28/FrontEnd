15058327308403d13847633163ed288aed98d748
yoel <yo3l.m18@gmail.com>
2017-02-23 13:57:48 -0400

    mode prod

--- appRoot/main.ts
+++ appRoot/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {AppModule} from "./app.module";
+import {enableProdMode} from "@angular/core";
 
 var adminLTE = require('adminLTE');
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then(success => console.log("Bootstrap success main"))
