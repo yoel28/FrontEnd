64a763214d4dbdfd8632e8075545c54601648bf2
Alex <riveram@zippyttech.com>
2017-03-01 17:43:39 -0400

    inputmask

--- appRoot/com.zippyttech.utils/directive/inputMask.ts
+++ appRoot/com.zippyttech.utils/directive/inputMask.ts
@@ -5,24 +5,46 @@ export interface IParamsInputMask{
 
 }
 var jQuery = require('jquery');
+var mask = require('inputmask');
 @Directive({
     selector: "[input-mask]",
-    inputs:['key','data','model','params'],
+    inputs:['data','rule','params'],
 })
 export class InputMask implements OnInit{
-    private key:string;
     private data:Object={};
-    private model:ModelRoot;
+    private rule:Object;
     private params:IParamsInputMask;
 
+    private expMask={
+        'email':[]
+        //'email':[]
+    };
 
     constructor(public el: ElementRef) {
-
+        /*jQuery.inputmask.extendDefinitions({
+            'f': {  //masksymbol
+                "validator": "[0-9\(\)\.\+/ ]",
+                "cardinality": 1,
+                'prevalidator': null
+            },
+        })*/
     }
 
     ngOnInit(){
         this.checkParams();
-        jQuery(this.el.nativeElement).knob(this.params);
+        if(this.rule['type'] == 'email'){
+            jQuery(this.el.nativeElement).inputmask("A{1,20}@B{1,20}.a{2,3}[.a{1,3}]",{
+                definitions:{
+                    'A':{validator:'[a-zA-Z0-9_.\-]'},
+                    'B':{validator:'[a-zA-Z0-9\-]'},
+                }
+            });
+        }
+        if(this.rule['type'] == 'phone'){
+            jQuery(this.el.nativeElement).inputmask("(99) 9999[9]-9999");
+        }
+
+
 
     }
     checkParams(){
