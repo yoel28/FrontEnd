320fbaff97402b41e5760669931a13e41b715f06
yoel <yo3l.m18@gmail.com>
2017-03-02 10:35:45 -0400

    oncomplete onincomplete

--- appRoot/com.zippyttech.utils/directive/inputMask.ts
+++ appRoot/com.zippyttech.utils/directive/inputMask.ts
@@ -1,5 +1,6 @@
 import {ElementRef, Directive,  OnInit} from "@angular/core";
 import {ModelRoot} from "../../com.zippyttech.common/modelRoot";
+import {FormControl} from "@angular/forms";
 
 export interface IParamsInputMask{
 
@@ -10,10 +11,10 @@ var bootstrap = require('bootstrap');
 
 @Directive({
     selector: "[input-mask]",
-    inputs:['data','rule','params'],
+    inputs:['control','rule','params'],
 })
 export class InputMask implements OnInit{
-    private data:Object={};
+    private control:FormControl;
     private rule:Object;
     private params:IParamsInputMask;
 
@@ -23,13 +24,7 @@ export class InputMask implements OnInit{
     };
 
     constructor(public el: ElementRef) {
-        /*jQuery.inputmask.extendDefinitions({
-            'f': {  //masksymbol
-                "validator": "[0-9\(\)\.\+/ ]",
-                "cardinality": 1,
-                'prevalidator': null
-            },
-        })*/
+        this.control = new FormControl();
     }
 
     ngOnInit(){
@@ -39,17 +34,51 @@ export class InputMask implements OnInit{
                 definitions:{
                     'A':{validator:'[a-zA-Z0-9_.\-]'},
                     'B':{validator:'[a-zA-Z0-9\-]'},
-                }
+                },
+                oncomplete: ((event)=>{
+                    let value = event.target.value;
+                    if(value){
+                        this.control.setValue(value)
+                    }
+                }).bind(this),
+                onincomplete: ((event)=>{
+                    this.control.setValue(null)
+                }).bind(this)
+
             });
         }
         if(this.rule['type'] == 'phone'){
             jQuery(this.el.nativeElement).inputmask("(99) 9999[9]-9999");
         }
         if(this.rule['type'] == 'date'){
-            jQuery(this.el.nativeElement).inputmask('dd-mm-yyyy');
+            jQuery(this.el.nativeElement).inputmask('dd-mm-yyyy',{
+                oncomplete: ((event)=>{
+                    let value = event.target.value;
+                    if(value){
+                        this.control.setValue(value)
+                    }
+                }).bind(this),
+                onincomplete: ((event)=>{
+                    this.control.setValue(null)
+                }).bind(this)
+
+            });
         }
         if(this.rule['type'] == 'datetime'){
-            jQuery(this.el.nativeElement).inputmask('dd-mm-yyyy hh:mm:ss');
+            jQuery(this.el.nativeElement).inputmask('dd-mm-yyyy hh:mm:ss',
+                {
+                    oncomplete: ((event)=>{
+                        let value = event.target.value;
+                        if(value){
+                            this.control.setValue(value)
+                        }
+                    }).bind(this),
+                    onincomplete: ((event)=>{
+                        this.control.setValue(null)
+                    }).bind(this)
+
+                }
+            );
         }
 
 
