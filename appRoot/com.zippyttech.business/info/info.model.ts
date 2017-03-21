8c71102e9853dd92c2ed7bebe031f8f57ebd196c
Alex <riveram@zippyttech.com>
2017-03-21 13:45:52 -0400

    initModelActions changed

--- appRoot/com.zippyttech.business/info/info.model.ts
+++ appRoot/com.zippyttech.business/info/info.model.ts
@@ -118,8 +118,8 @@ export class InfoModel extends ModelBase{
         this.rulesSave = Object.assign({},this.rules);
         delete this.rulesSave.enabled;
     }
-    initModelActions(params){
-        params['delete'].message='¿Esta seguro de eliminar la ayuda : ';
+    initModelActions(){
+        this.actions['delete'].params.message='¿Esta seguro de eliminar la ayuda : ';
     }
 
 }
