4c4e3a06e1ee14c2b9ee31ed8f4b17ec39bdaa37
yoel <yo3l.m18@gmail.com>
2017-03-23 14:42:38 -0400

    rule image

--- appRoot/com.zippyttech.common/rules/image.rule.ts
+++ appRoot/com.zippyttech.common/rules/image.rule.ts
@@ -1,11 +1,20 @@
 import {IRule, Rule} from "./rule";
+import {StaticValues} from "../../com.zippyttech.utils/catalog/staticValues";
 
 export interface IImage  extends IRule{
     default?:string;
 }
 export class ImageRule extends Rule{
 
-    constructor(public rule:IImage){
+    constructor(private rule:IImage){
         super(rule);
     }
+
+    get default():string{
+        return this.attributes.default || StaticValues.pathElements.robot;
+    }
+    set default(value:string){
+        this.attributes.default = value;
+    }
+
 }
