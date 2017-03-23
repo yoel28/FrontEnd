d29a12f2627d33645a1f877931a1facd10db5533
yoel <yo3l.m18@gmail.com>
2017-03-23 14:41:08 -0400

    rule date

--- appRoot/com.zippyttech.common/rules/combodate.rule.ts
+++ appRoot/com.zippyttech.common/rules/combodate.rule.ts
@@ -5,7 +5,15 @@ export interface ICombodate  extends IRule{
 }
 export class CombodateRule extends Rule{
 
-    constructor(public rule:ICombodate){
+    constructor(private rule:ICombodate){
         super(rule);
     }
+
+    get date():string{
+        return this.attributes.date || 'date';
+    }
+    set date(value:string){ //TODO: eval value in date or datetime
+        this.attributes.date = value;
+    }
+
 }
