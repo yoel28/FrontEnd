9a302a3ac3bd1beed9165b7a5cf0afaec016695a
yoel <yo3l.m18@gmail.com>
2017-03-27 10:02:29 -0400

    dashboard test

--- appRoot/com.zippyttech.init/dashboard/dashboard.component.ts
+++ appRoot/com.zippyttech.init/dashboard/dashboard.component.ts
@@ -1,9 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Http} from '@angular/http';
-import {ModalService} from "../../com.zippyttech.services/modal/modal.service";
-import {UserModel} from "../../com.zippyttech.access/user/user.model";
 import {DependenciesBase} from "../../com.zippyttech.common/DependenciesBase";
-import {ModalParams, IModalDelete, IModalSave} from "../../com.zippyttech.services/modal/modal.types";
 
 declare var SystemJS:any;
 
@@ -14,29 +10,9 @@ declare var SystemJS:any;
     styleUrls: ['style.css']
 })
 export class DashboardComponent implements OnInit{
-    userModel:any = {};
-    modals:{name:string, params:ModalParams}[] = [];
+    constructor(public db:DependenciesBase) {}
 
-    constructor(public db:DependenciesBase,public http:Http,private ms:ModalService) {
-    }
-
-    ngOnInit():void{
-        this.userModel = new UserModel(this.db);
-        this.userModel.loadData().then((()=> {
-            this.modals.push({
-                    name: 'delete',
-                    params:{ model: this.userModel, selectedData: this.userModel.dataList.list[0]}
-                },{
-                    name: 'save',
-                    params:{ model: this.userModel, afterRunning:()=>{alert("exit");}}
-                }
-            );
-        }).bind(this));
-    }
-
-    public getObjectKeys(data):string[] {
-        return Object.keys(data || {});
-    }
+    ngOnInit():void{}
 
 }
 
