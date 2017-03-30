e88e3acb97dc4c1bb50e13bb6359176c89f76514
yoel <yo3l.m18@gmail.com>
2017-03-30 09:30:21 -0400

    remove system-config

--- clear.sh
+++ clear.sh
@@ -3,9 +3,7 @@ find ./appRoot -type f -name '*.js' -exec rm -f {} \;
 find ./appRoot -type f -name '*.js.map' -exec rm -f {} \;
 find ./appRoot -type f -name '*.d.ts' -exec rm -f {} \;
 find ./appRoot -type f -name '*.gz*' -exec rm -f {} \;
-rm -f ./system-config.js;
-rm -f ./system-config.js.map;
-rm -f ./build.js.map;
+rm -f ./build.js;
 rm -f ./build.js.map;
 rm -rf dist/
 rm -rf aot/
