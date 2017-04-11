e6fe525ce0d72eff6917c2b0c864700888bce872
yoel <yo3l.m18@gmail.com>
2017-04-11 13:44:40 -0400

    list rule

---
+++ appRoot/com.zippyttech.common/rules/list.rule.ts
@@ -0,0 +1,28 @@
+import {IRule, Rule} from "./rule";
+import {TagsInput} from "../../com.zippyttech.utils/directive/tagsinput";
+
+export interface IList  extends IRule{
+    instance?:TagsInput;
+    inputFree?:boolean;
+}
+export class  ListRule extends Rule{
+
+    constructor(private rule:IList){
+        super(rule);
+    }
+
+    get instance():TagsInput{
+        return this.attributes.instance || null;
+    }
+    set instance(value:TagsInput){
+       this.attributes.instance = value;
+    }
+
+    get inputFree():boolean{
+        return this.attributes.inputFree || false;
+    }
+    set inputFree(value:boolean){
+        this.attributes.inputFree = value;
+    }
+
+}
