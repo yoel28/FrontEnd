0d693121cdc1563eee16afd5c26911eb3a438b08
yoel <yo3l.m18@gmail.com>
2017-04-11 13:13:28 -0400

    url rule

---
+++ appRoot/com.zippyttech.common/rules/url.rule.ts
@@ -0,0 +1,20 @@
+import {IRule, Rule} from "./rule";
+
+type urlOptions = 'image'|'link';
+export interface IUrl  extends IRule{
+    url?:urlOptions;
+}
+export class UrlRule extends Rule{
+
+    constructor(private rule:IUrl){
+        super(rule);
+    }
+
+    get url():urlOptions{
+        return this.attributes.url || 'link';
+    }
+    set url(value:urlOptions){
+        this.attributes.url = value;
+    }
+
+}
