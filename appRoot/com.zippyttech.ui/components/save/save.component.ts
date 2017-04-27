9316833ed870b54c9e02229e45507f0f05c631b3
yoel <yo3l.m18@gmail.com>
2017-04-27 09:05:49 -0400

    update save v2 incompleted

--- appRoot/com.zippyttech.ui/components/save/save.component.ts
+++ appRoot/com.zippyttech.ui/components/save/save.component.ts
@@ -2,124 +2,142 @@ import {Component, EventEmitter, OnInit, AfterViewInit} from "@angular/core";
 import {FormComponent} from "../form/form.component";
 import {DependenciesBase} from "../../../com.zippyttech.common/DependenciesBase";
 import {ModelRoot} from "../../../com.zippyttech.common/modelRoot";
+import {TModels, TRules} from "../../../app-routing.module";
+import {ObjectRule} from "../../../com.zippyttech.common/rules/object.rule";
 
 export interface ISave{
     model:ModelRoot;
     childKey?:string;
-    onlyRequired?:boolean;
-    update?:boolean;
 }
 
 @Component({
     selector: 'save-view',
     templateUrl: './index.html',
     styleUrls: ['./style.css'],
-    inputs:['params'],
-    outputs:['getInstance']
+    inputs:['params']
 })
 export class SaveComponent implements OnInit,AfterViewInit{
 
     public params:ISave;
-    public instanceForm:FormComponent;
     public save:EventEmitter<Object>;
-    public getInstance:EventEmitter<SaveComponent>;
-    public childModel?:ModelRoot;
+
+    private _form:FormComponent;
+    private _childModel:ModelRoot;
 
 
     constructor(public db:DependenciesBase) {
         this.save = new EventEmitter();
-        this.getInstance = new EventEmitter<SaveComponent>();
     }
+
+    //region hooks ng
 
     ngOnInit(){
-        this.initChildModel();
-        console.log(this.params.model);
-        console.log(this.childModel);
-        console.log(this.params.childKey);
-
+        this._initModelChild();
     }
 
-    setForm(form:FormComponent){
-        this.instanceForm=form;
+    ngAfterViewInit(){}
+
+    //endregion
+
+    //region admin form
+
+    private _setForm(form:FormComponent){
+        this._form=form;
     }
 
-    ngAfterViewInit(){
-        this.getInstance.emit(this);
-    }
-
-    private initChildModel(){
-        if(this.params && this.params.model && this.params.model instanceof ModelRoot)
-            if(this.params.childKey)
-                if(
-                    this.params.model.rules[this.params.childKey] &&
-                    this.params.model.rules[this.params.childKey].model &&
-                    this.params.model.rules[this.params.childKey].model instanceof ModelRoot
-                )
-                {
-                    this.childModel = this.params.model.rules[this.params.childKey].model;
-                }
-                else this.db.debugLog(['Error:02 SaveComponent -> loadModels',this.params.childKey,this.params.model]);
-        else this.db.debugLog(['Error:01 SaveComponent -> loadModels',this.params.childKey,this.params.model]);
-    }
-
-    private get currentModel():ModelRoot{
-        return this.childModel || this.params.model;
-    }
-
-    private submitForm(event?,addBody=null){
+    private _fnFormSubmit(event?:Event){
         if(event)
             event.preventDefault();
 
-        let successCallback = (response => {
-            this.currentModel.addToast('Notificacion','Guardado con éxito');
-            this.instanceForm.resetForm();
+        let successCallback = (response) => {
             this.save.emit(response.json());
             if(this.params.childKey)//TODO:Arreglar
-                this.params.model.afterSave(this.params.childKey,this.params.model.currentData,response.json().id);
+            {
+                // this.params.model.afterSave(this.params.childKey,this.params.model.currentData,response.json().id);
+            }
 
-        }).bind(this);
-        let body = this.instanceForm.getFormValues(addBody);
-        if(!this.params.update)
-            this.currentModel.onSave(body,successCallback);
-        else
-            this.currentModel.onPatchObject(body,this.currentModel['currentData']);
+        };
+
+        let body = this._form.getFormValues();
+        // if(!this.params.update)
+        //     (<ModelRoot>this._currentModel).onSave(body,successCallback);
+        // else
+        //     (<ModelRoot>this._currentModel).onPatchObject(body,this._currentModel['currentData']);
+
+        (<ModelRoot>this._currentModel).onSave(body,successCallback);
         if(this.db.ms.currentModal == 'save')
             this.db.ms.hideCurrentModal();
     }
 
+    private _fnFormValid():boolean{
+        if(this._form && this._form.valid)
+            return this._form.valid;
+        return false;
+    }
+
+
+    //endregion
+
+    //region others
+
+    private _getRule(key:string):TRules{
+        return this.params.model.rules[key] || {};
+    }
+
+
+    //endregion
+
+
+    //region models
+
+    private _initModelChild(){
+        if(this.params && this.params.model && this.params.model instanceof ModelRoot)
+        {
+            let child = this.params.childKey;
+            if(child){
+                if((<ObjectRule>this._getRule(child)).model instanceof ModelRoot)
+                {
+                    this._childModel = (<ObjectRule>this._getRule(child)).model;
+                    return;
+                }
+                this.db.debugLog('Error:02 SaveComponent -> loadModels',this.params.childKey,this.params.model);
+            }
+            return;
+        }
+        this.db.debugLog('Error:01 SaveComponent -> loadModels',this.params.childKey,this.params.model);
+
+    }
+
+    private get _currentModel():TModels{
+        return this._childModel || this.params.model;
+    }
+
+    //endregion
+
+
     formActions(){
-        if(this.params.update)
-            return [{'title':'Actualizar','class':'btn btn-blue','icon':'fa fa-save','addBody':null}];
+        // if(this.params.update)
+        //     return [{'title':'Actualizar','class':'btn btn-blue','icon':'fa fa-save','addBody':null}];
 
         return [{'title':'Registrar','class':'btn btn-primary','icon':'fa fa-save','addBody':null}]
     }
 
-    formValid():boolean{
-        if(this.instanceForm && this.instanceForm.form)
-            return this.instanceForm.isValidForm();
-        return false;
-    }
+
 
     setLoadDataModel(data,_delete=false) {
-        this.instanceForm.setLoadDataModel(data,_delete);
-    }
-
-    existloadSearch():boolean{
-        if(this.instanceForm && this.instanceForm.search && this.instanceForm.search.object)
-            return this.instanceForm.searchView;
-        return false;
+        // this.instanceForm.setLoadDataModel(data,_delete);
     }
 
     existloadDelete():boolean {
-        if(this.instanceForm && this.instanceForm.delete && this.instanceForm.rest.id && parseFloat(this.instanceForm.rest.id || '-1')>0 )
-            return true;
+        // if(this.instanceForm && this.instanceForm.id && parseFloat(this.instanceForm.getRest().id || '-1')>0 )
+        //     return true;
         return false;
     }
 
     loadDelete(event){
         if(event)
             event.preventDefault();
-        this.currentModel.onDelete(event,this.currentModel.rest.id);
+        (<ModelRoot>this._currentModel).onDelete(event,(<ModelRoot>this._currentModel).getRest().id);
         if(this.db.ms.currentModal == 'save')
             this.db.ms.hideCurrentModal();
     }
