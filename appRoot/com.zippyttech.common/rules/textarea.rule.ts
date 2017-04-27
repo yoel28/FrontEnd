e9480d24f4efc6673073929e39aa3009a75c7329
yoel <yo3l.m18@gmail.com>
2017-04-27 08:31:22 -0400

    add rows in interface

--- appRoot/com.zippyttech.common/rules/textarea.rule.ts
+++ appRoot/com.zippyttech.common/rules/textarea.rule.ts
@@ -1,11 +1,19 @@
 import {Editable, IEditable} from "./editable.types";
+import {API} from "../../com.zippyttech.utils/catalog/defaultAPI";
 
 export interface ITextarea  extends IEditable{
-
+    rows?:number;
 }
 export class TextareaRule extends Editable{
 
     constructor(private rule:ITextarea){
         super(rule);
+    }
+
+    set rows(value:number){
+        this.attributes.rows = value;
+    }
+    get rows():number{
+        return this.attributes.rows || API.TEXTAREA_ROWS
     }
 }
