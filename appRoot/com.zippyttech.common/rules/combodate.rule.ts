b28128b434b5df8010f86c189c47ec9d0e4d43b4
yoel <yo3l.m18@gmail.com>
2017-04-28 14:29:28 -0400

    add        format?:string;         viewformat?:string;         template?:string;         minYear?:number;         maxYear?:number;

--- appRoot/com.zippyttech.common/rules/combodate.rule.ts
+++ appRoot/com.zippyttech.common/rules/combodate.rule.ts
@@ -3,6 +3,11 @@ import {IEditable, Editable} from "./editable.types";
 type dateOptions = 'date' | 'datetime';
 export interface ICombodate  extends IEditable{
     date:dateOptions;
+    format?:string;
+    viewformat?:string;
+    template?:string;
+    minYear?:number;
+    maxYear?:number;
 }
 
 export class CombodateRule extends Editable{
@@ -16,6 +21,41 @@ export class CombodateRule extends Editable{
     }
     set date(value:dateOptions){
         (<ICombodate>this.attributes).date = value;
+    }
+
+    get format():string{
+        return this.attributes.format;
+    }
+    set format(value:string){
+        (<ICombodate>this.attributes).format = value;
+    }
+
+    get viewformat():string{
+        return this.attributes.viewformat;
+    }
+    set viewformat(value:string){
+        (<ICombodate>this.attributes).viewformat = value;
+    }
+
+    get template():string{
+        return this.attributes.template;
+    }
+    set template(value:string){
+        (<ICombodate>this.attributes).template = value;
+    }
+
+    get minYear():number{
+        return this.attributes.minYear;
+    }
+    set minYear(value:number){
+        (<ICombodate>this.attributes).minYear = value;
+    }
+
+    get maxYear():number{
+        return this.attributes.maxYear;
+    }
+    set maxYear(value:number){
+        (<ICombodate>this.attributes).maxYear = value;
     }
 
 }
