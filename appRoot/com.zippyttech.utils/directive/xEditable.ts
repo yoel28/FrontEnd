88f02dc7165067f7ce67eba39dc1842b3b2a1438
yoel <yo3l.m18@gmail.com>
2017-04-28 14:28:16 -0400

    update editable

--- appRoot/com.zippyttech.utils/directive/xEditable.ts
+++ appRoot/com.zippyttech.utils/directive/xEditable.ts
@@ -4,6 +4,11 @@ import {ModelRoot} from "../../com.zippyttech.common/modelRoot";
 import {TRules} from "../../app-routing.module";
 import {API} from "../catalog/defaultAPI";
 import {Editable} from "../../com.zippyttech.common/rules/editable.types";
+import {ObjectRule} from "../../com.zippyttech.common/rules/object.rule";
+import {BooleanRule} from "../../com.zippyttech.common/rules/boolean.rule";
+import {SelectRule} from "../../com.zippyttech.common/rules/select.rule";
+import {CombodateRule} from "../../com.zippyttech.common/rules/combodate.rule";
+import {NumberRule} from "../../com.zippyttech.common/rules/number.rule";
 
 var jQuery = require('jquery');
 var bootstrap = require('bootstrap');
@@ -29,8 +34,7 @@ export class XEditable implements OnInit {
     public model:ModelRoot;
     public key:string;
     public data:Object;
-
-
+    public endpoint:string;
 
     constructor(public el:ElementRef, public db:DependenciesBase) {}
 
@@ -43,9 +47,8 @@ export class XEditable implements OnInit {
 
             type: this._getType(currentRule),
             value: this._getValue(currentRule),
-            defaultValue: this._getDefaultValue(currentRule),
+            emptytext: this._getEmptyText(currentRule),
             disabled: this._getDisabled(currentRule),
-            display: this._getDisplay(currentRule),
             showbuttons: this._getShowbuttons(currentRule),
             mode: this._getMode(currentRule),
             source:this._getSource(currentRule),
@@ -57,20 +60,23 @@ export class XEditable implements OnInit {
             placement:this._getPlacement(currentRule),
 
             validate: (newValue) =>{
-                let msg = this._getIsValid(currentRule,newValue);
-                if(msg)
-                    return msg;
+                let error = this._getIsValid(currentRule,newValue);
+                if(error){
+                    return this.db.getTranslate(error.code,error.params);
+                }
+                else {
+                    newValue = this._fnParseValueValidate(currentRule,newValue);
 
-                newValue = this._fnParseValueValidate(currentRule,newValue);
+                    this.model.onPatch(this.key,this.data,newValue).then(
+                        (value)=> {
+                            this._evSetValue(currentRule,value[this.key]);
+                        },
+                        (reason) => {
+                            this._evSetValue(currentRule);
+                        }
+                    );
+                }
 
-                this.model.onPatch(this.key,this.data,newValue).then(
-                    (value)=> {
-                        this._evSetValue(currentRule,value[this.key]);
-                    },
-                    (reason) => {
-                        this._evSetValue(currentRule);
-                    }
-                );
             }
         });
     }
@@ -79,19 +85,19 @@ export class XEditable implements OnInit {
         return this.model.rules[this.key];
     }
 
-    private _getType(rule):string{
+    private _getType(rule:TRules):string{
         let type = rule.type;
         switch (type) {
             case "url" :
                 return 'url';
             case "object":
-                return rule.mode;
+                return (<ObjectRule>rule).mode;
             default :
                 return type || 'text'
         }
     }
 
-    private _getValue(rule){
+    private _getValue(rule:TRules){
         switch (this._getType(rule)) {
             case "combodate" :
                 if(this.data[this.key])
@@ -102,32 +108,30 @@ export class XEditable implements OnInit {
             case "url" :
                 return 'link';
             default :
-                return (rule.subKey?( this.data[this.key][rule.subKey] || 'N/A' ):( this.data[this.key] || "N/A"));
+                return this.data[this.key];
         }
     }
 
-    private _getDefaultValue(rule){
+    private _getEmptyText(rule:TRules){
         switch (this._getType(rule)) {
             case "combodate" :
                 return null;
-            case "password" :
-                return '';
+            case "password":
+                return '******';
             case "url" :
                 return 'link';
+            case "object":
+                return (<ObjectRule>rule)
             default :
-                return 'N/A';
+                return (<Editable>rule).emptyText || 'N/A';
         }
     }
 
-    private _getDisabled(rule){
+    private _getDisabled(rule:TRules){
         return this.disabled;
     }
 
-    private _getDisplay(rule){
-        return rule.display || null
-    }
-
-    private _getShowbuttons(rule){
+    private _getShowbuttons(rule:TRules){
         if(this._getMode(this.getRule) == 'popup')
             return true;
 
@@ -137,68 +141,68 @@ export class XEditable implements OnInit {
             case "textarea" :
                 return true;
             case "object":{
-                if(rule.mode == 'checklist')
+                if((<ObjectRule>rule).mode == 'checklist')
                     return true;
             }
             case "checklist":
                 return true;
-            default :
-                return rule.showbuttons || false;
+            default:
+                return (<Editable>rule).showbuttons || false;
         }
     }
 
-    private _getMode(rule){
+    private _getMode(rule:TRules){
         switch (this._getType(rule)) {
             case "combodate" :
-                return rule.mode || 'popup';
+                return (<Editable>rule).mode || 'popup';
             case "textarea" :
-                return rule.mode || 'popup';
+                return (<Editable>rule).mode || 'popup';
             default :
-                return rule.mode || 'inline';
+                return (<Editable>rule).mode || 'inline';
         }
     }
 
-    private _getSource(rule){
-        return rule.source || null
+    private _getSource(rule:TRules){
+        return (<BooleanRule | SelectRule>rule).source || null
     }
 
-    private _getFormat(rule){
+    private _getFormat(rule:TRules){
         switch (this._getType(rule)) {
             case "combodate" :
-                if(rule.date == 'date'){
-                    return rule.format || 'YYYY-MM-DD'
+                if( (<CombodateRule>rule).date == 'date'){
+                    return (<CombodateRule>rule).format || 'YYYY-MM-DD'
                 }
-                if(rule.date == 'datetime'){
-                    return rule.format || 'YYYY-MM-DD HH:mm'
+                if((<CombodateRule>rule).date == 'datetime'){
+                    return (<CombodateRule>rule).format || 'YYYY-MM-DD HH:mm'
                 }
             default :
                 return null;
         }
     }
 
-    private _getViewFormat(rule){
+    private _getViewFormat(rule:TRules){
         switch (this._getType(rule)) {
             case "combodate" :
-                if(rule.date == 'date'){
-                    return rule.viewformat || 'MMM D, YYYY';
+                if( (<CombodateRule>rule).date == 'date'){
+                    return (<CombodateRule>rule).viewformat || 'MMM D, YYYY';
                 }
-                if(rule.date == 'datetime'){
-                    return rule.viewformat || 'MMM D, YYYY, HH:mm';
+                if( (<CombodateRule>rule).date == 'datetime'){
+                    return (<CombodateRule>rule).viewformat || 'MMM D, YYYY, HH:mm';
                 }
             default :
                 return null;
         }
     }
 
-    private _getTemplate(rule){
+    private _getTemplate(rule:TRules){
 
         switch (this._getType(rule)) {
             case "combodate" :
-                if(rule.date == 'date'){
-                    return rule.template || 'D MMM YYYY';
+                if( (<CombodateRule>rule).date == 'date'){
+                    return (<CombodateRule>rule).template || 'D MMM YYYY';
                 }
-                if(rule.date == 'datetime'){
-                    return rule.template || 'D MMM YYYY  HH:mm';
+                if( (<CombodateRule>rule).date == 'datetime'){
+                    return (<CombodateRule>rule).template || 'D MMM YYYY  HH:mm';
                 }
             default :
                 return null;
@@ -206,27 +210,27 @@ export class XEditable implements OnInit {
 
     }
 
-    private _getStep(rule){
-        return rule.step || "0.001"
+    private _getStep(rule:TRules){
+        return (<NumberRule>rule).step || "0.001"
     }
 
-    private _getCombodate(rule){
+    private _getCombodate(rule:TRules){
         switch (this._getType(rule)) {
             case "combodate" :
                 return {
-                        minYear: this.db.myglobal.getParams('MIN_DATE',API.MIN_DATE),
-                        maxYear: this.db.myglobal.getParams('MAX_DATE',API.MAX_DATE),
+                        minYear: (<CombodateRule>rule).minYear || this.db.myglobal.getParams('MIN_DATE',API.MIN_DATE),
+                        maxYear: (<CombodateRule>rule).maxYear ||this.db.myglobal.getParams('MAX_DATE',API.MAX_DATE),
                     };
             default :
                 return null;
         }
 
     }
-    private _getPlacement(rule){
+    private _getPlacement(rule:TRules){
         return (<Editable>this.getRule).placement || 'top';
     }
 
-    private _evSetValue(rule,data=null){
+    private _evSetValue(rule:TRules,data=null){
         switch (this._getType(rule)) {
             case "url" :
                 jQuery(this.el.nativeElement).editable('setValue','link', true);
@@ -239,31 +243,32 @@ export class XEditable implements OnInit {
         }
     }
 
-    private _getIsValid(rule,newValue){
+    private _getIsValid(rule:TRules,newValue:any):any{
+
         if(rule.required && !newValue)
-            return this.db.msg.required;
+            return {code:'error.required',params:{}};
 
         if(rule.maxLength && newValue && newValue.length > rule.maxLength )
-            return this.db.msgParams('errorMaxlength',[rule.maxLength]);
+            return {code:'error.maxlength',params:{value:rule.maxLength}};
 
         if(rule.minLength && newValue && newValue.length < rule.minLength )
-            return this.db.msgParams('errorMinlength',[rule.minLength]);
+            return {code:'error.minlength',params:{value:rule.minLength}};
 
+        return null;
     }
 
-    private _fnParseValueValidate(rule,newValue){
+    private _fnParseValueValidate(rule:TRules,newValue:any){
         switch (this._getType(rule)) {
             case "combodate" :
-                if(rule.date == 'date'){
+                if((<CombodateRule>rule).date == 'date'){
                     return newValue.format('YYYY-MM-DD');
                 }
-                if(rule.date == 'datetime'){
+                if((<CombodateRule>rule).date == 'datetime'){
                     return newValue.format('YYYY-MM-DD HH:mmZZ');
                 }
             default :
                 return newValue;
         }
     }
-
 
 }
